e0d2210d76b869230329ef024a862144ee68b40f
AdedoyinAbike <habeebahadedoyinoyatayo @gmail.compwdgit config --global user.emailpwdcdgit config --global user.email habeebahadedoyinoyatayo>
2023-11-08 18:53:24 +0000

    firsthigher level programming code

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(f"{str[str.index('object-oriented programming')].strip()}")
+str = str.replace('object-oriented programming', 'object-oriented programming\n')
 print(str)
