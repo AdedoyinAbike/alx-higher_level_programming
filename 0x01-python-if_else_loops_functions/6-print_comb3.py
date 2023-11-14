eaaaf5845beb3be7a1479b9a3b98f97576e77f2c
AdedoyinAbike <habeebahadedoyinoyatayo @gmail.compwdgit config --global user.emailpwdcdgit config --global user.email habeebahadedoyinoyatayo>
2023-11-14 10:03:57 +0000

    updated

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i + 1, 10):
-        print('{}{}'.format(i,j) if i < j else '{}{}'.format(j,i), end=', ')
+        print('{:02d}'.format(i * 10 + j), end=', 'if i < 8 else '\n')
