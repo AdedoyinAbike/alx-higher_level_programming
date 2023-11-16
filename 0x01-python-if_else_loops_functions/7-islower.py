e5744a195380f3410cc899c999cdf392e051daf0
AdedoyinAbike <habeebahadedoyinoyatayo @gmail.compwdgit config --global user.emailpwdcdgit config --global user.email habeebahadedoyinoyatayo>
2023-11-16 18:21:26 +0000

    updated

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
     return 'a' <= c <= 'z'
-char_input = input('enter a character: ')
+char_input = input('enter a c: ')
 if islower(char_input):
     print(f'{char_input} is lower')
 else:
