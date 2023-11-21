16ecb8ee1e9ab3419c0944c2c011b45d13bd8d31
AdedoyinAbike <habeebahadedoyinoyatayo @gmail.compwdgit config --global user.emailpwdcdgit config --global user.email habeebahadedoyinoyatayo>
2023-11-21 20:15:20 +0000

    updated

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
     return 'a' <= c <= 'z'
-def islower(c):
+def islower():
     return 'a' <= c <= 'z'
 print('a => {}'.format('lower' if islower('a') else 'upper'))
 islower(c)
